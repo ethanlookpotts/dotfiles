cac82bf018beb304d609df35fba102046b066383
Ethan Look-Potts <ethanlookpotts@gmail.com>
2026-04-09 17:03:56 -0400

    fix: no package lock when building markdown preview

--- .config/nvim/lua/ethanlookpotts/plugins/markdown.lua
+++ .config/nvim/lua/ethanlookpotts/plugins/markdown.lua
@@ -2,7 +2,7 @@ return {
   "iamcco/markdown-preview.nvim",
   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
   ft = { "markdown" },
-  build = "sh -c 'cd app && ./install.sh'",
+  build = "cd app && npm install --no-package-lock",
   init = function()
     vim.g.mkdp_filetypes = { "markdown" }
   end,
